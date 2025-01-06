009a265105755c8f6321767c9fa123bc4a3b2a45
Arthur LAMARD <arthur.lamard@chu-brest.fr>
2025-01-06 15:15:20 +0100

    refacto module

--- incognito/__main__.py
+++ incognito/__main__.py
@@ -1,1 +1,28 @@
-print("Hello Incognito")
+from . import anonymizer
+
+infos = {
+    "first_name": "Arthur",
+    "last_name": "Lamard",
+    "birth_name": "",
+    "birthdate": "2000-12-07",
+    "ipp": "0987654321",
+    "postal_code": "29820"
+
+}
+
+
+text = """
+    Bonjour, je suis Arthur Lamard, je suis ingénieur au CDC, né le 07/12/2000, j'ai actuellement 24 ans.
+    Je suis venu au CHU pour un rdv médical pour un mal de genou. L'IPP 0987654321 m'a été attribué. 
+    je vis à Guilers, 29820. 
+    je suis contactable aux coordonnées suivantes : 
+    email: arthur@lamard.org 
+    telephone : 0647187486
+
+    
+"""
+ano = anonymizer.Anonymizer(text=text)
+# ano = ano.run_dummy_algorithm(text=text, info=infos)
+if __name__ == '__main__':
+    test = ano.anonymize_text()
+    print(test)
