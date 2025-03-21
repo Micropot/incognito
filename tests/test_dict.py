fc81401930bd85f1a64f89c3724927fdca752a5f
Arthur LAMARD <arthur.lamard@chu-brest.fr>
2025-03-21 11:51:10 +0100

    maj tests unitaires

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -1,4 +1,4 @@
-from incognito_anonymizer import PersonalInfo
+from incognito_anonymizer import PersonalInfo, anonymizer
 import pytest
 from datetime import datetime
 
@@ -36,10 +36,54 @@ def test_from_dict_valid_data():
         "postal_code": "75001",
         "adress": "456 Elm Street"
     }
-    info = PersonalInfo(**data)
 
+    info = PersonalInfo(**data)
     assert info.first_name == "Jane"
     assert info.last_name == "Doe"
+    assert info.birth_name == "Johnson"
+    assert info.birthdate == datetime(1985, 8, 25)
+    assert info.ipp == "987654321"
+    assert info.postal_code == "75001"
+    assert info.adress == "456 Elm Street"
+
+
+def test_from_dict_missing_name():
+    # Transformation d'un dictionnaire valide
+    data = {
+        "first_name": "Jane",
+        "last_name": None,
+        "birth_name": "Johnson",
+        "birthdate": datetime(1985, 8, 25),
+        "ipp": "987654321",
+        "postal_code": "75001",
+        "adress": "456 Elm Street"
+    }
+    ano = anonymizer.Anonymizer()
+    info = ano.set_info_from_dict(**data)
+    assert info.first_name == "Jane"
+    assert info.last_name == ""
+    assert info.birth_name == "Johnson"
+    assert info.birthdate == datetime(1985, 8, 25)
+    assert info.ipp == "987654321"
+    assert info.postal_code == "75001"
+    assert info.adress == "456 Elm Street"
+
+
+def test_from_dict_missing_name():
+    # Transformation d'un dictionnaire valide
+    data = {
+        "first_name": "Jane",
+        "last_name": None,
+        "birth_name": "Johnson",
+        "birthdate": datetime(1985, 8, 25, 00, 00),
+        "ipp": "987654321",
+        "postal_code": "75001",
+        "adress": "456 Elm Street"
+    }
+    ano = anonymizer.Anonymizer()
+    info = ano.set_info_from_dict(**data)
+    assert info.first_name == "Jane"
+    assert info.last_name == ""
     assert info.birth_name == "Johnson"
     assert info.birthdate == datetime(1985, 8, 25)
     assert info.ipp == "987654321"
