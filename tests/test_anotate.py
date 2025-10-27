9da55593a146d3984464d8a7fc71c2d62323413c
Arthur LAMARD <arthur.lamard@chu-brest.fr>
2025-10-27 12:19:37 +0100

    feat(tests)! : add unit test for better covering

--- tests/test_anotate.py
+++ tests/test_anotate.py
@@ -3,8 +3,8 @@ from incognito_anonymizer import anonymizer
 
 def test_standoff():
     ano = anonymizer.Anonymizer()
-    ano.set_annotator('standoff')
-    ano.add_analyzer('regex')
+    ano.set_annotator("standoff")
+    ano.add_analyzer("regex")
     input_text = "Dr. Bob"
     output_text = ano.annotate(input_text)
     assert output_text == "T1\tNAME 4 7\tBob"
@@ -12,8 +12,8 @@ def test_standoff():
 
 def test_doccano():
     ano = anonymizer.Anonymizer()
-    ano.set_annotator('doccano')
-    ano.add_analyzer('regex')
+    ano.set_annotator("doccano")
+    ano.add_analyzer("regex")
     input_text = "Dr. Bob"
     output_text = ano.annotate(input_text)
     assert output_text == '{"text": "Dr. Bob", "label": [[4, 7, "NAME"]]}'
